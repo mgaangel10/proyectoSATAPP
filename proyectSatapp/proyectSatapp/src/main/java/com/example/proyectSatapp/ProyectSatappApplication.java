3162c4b2d96da0c7dc9da41ef3064b9b674b6a59
mgaangel10 <mg.aangel10@gmail.com>
2023-12-25 18:31:02 +0100

    avances de seguridad

---
+++ proyectSatapp/proyectSatapp/src/main/java/com/example/proyectSatapp/ProyectSatappApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectSatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectSatappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectSatappApplication.class, args);
+	}
+
+}
